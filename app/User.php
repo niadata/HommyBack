e874ac7fa959306d11a143e5bc1c10b6f392daeb
Alexandra Gomes <65859099+XandraGomes@users.noreply.github.com>
2020-07-29 22:38:00 -0300

    relação

--- app/User.php
+++ app/User.php
@@ -10,6 +10,14 @@ use App\Http\Requests\UserRequest;
 class User extends Authenticatable
 {
     use Notifiable;
+    
+    public function republics(){
+        return $this->hasMany('App\Republic');
+    }
+
+    public function republic(){
+        return $this->hasOne('App\Republic');
+    }
 
     public function createUser(UserRequest $request){
         /*Função que cria um usuario*/
@@ -77,38 +85,6 @@ class User extends Authenticatable
     ];
 
 
-    // public function republic(){
-    //     return $this->belongsTo('App\Republic');
-    // } 
     
-    // // public function republics(){
-    // //     return $this->hasMany('App\Republic');
-    // // }
-    // public function repFavoriteUser(){
-    //     return $this->belongsToMany('App\Republic');
-    // }
-
-    // public function alugar($republic_id){
-    //     $republic = Republic::findOrFail($republic_id);
-    //     $this->republic_id = $republic_id;
-    //     $this->save();
-    // }
-    
-    
-    //    tem uma coluna para receber a chave estrangeira de republic
-    // retorna republica
-    // colocar norme republica
-    // remover republica
-    // favoritadas
        
 }
-
-
-
-
-
-// public function removeAluguel(){
-//     $this->republic_id = NULL;
-//     $this->save();
-// }
-
