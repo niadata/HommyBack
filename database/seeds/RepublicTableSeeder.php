fbbbb8981053d0549244425086e649cfd17eb215
Alexandra Gomes <65859099+XandraGomes@users.noreply.github.com>
2020-07-30 23:30:26 -0300

    Update RepublicTableSeeder.php

--- database/seeds/RepublicTableSeeder.php
+++ database/seeds/RepublicTableSeeder.php
@@ -11,6 +11,13 @@ class RepublicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Republic::class,5)->create()->each(function($republic){
+            $comments = factory(App\Comment::class, 2)->make();
+            $user = factory(App\User::class)->make();
+            
+            $republic->Comment()->saveMany($comments);
+            $republic->users()->save($user);
+            $user->republics()->attach($reoublic);
+        });
     }
 }
